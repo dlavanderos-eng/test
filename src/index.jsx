b3835bcfe5dfcb733060ea1ec303d97530ad48b0
Lavanderos Ramírez <dlavanderos@cenabast.local>
2026-07-20 17:34:15 -0400

    cambiar configuraciones de tailwind y postcss a commonjs para webpack

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 function App() {
   const [usuarios, setUsuarios] = useState([]);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,html,jsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
